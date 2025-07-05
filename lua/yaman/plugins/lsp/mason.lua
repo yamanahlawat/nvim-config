b949b572764429b45d04e140f94712ebe33ce36f
Yaman Ahlawat <yaman@outlook.com>
2025-07-05 08:40:41 +0530

    feat: added gitsigns, lazygit, formatting, linting

--- lua/yaman/plugins/lsp/mason.lua
+++ lua/yaman/plugins/lsp/mason.lua
@@ -25,19 +25,19 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "ts_ls",         -- typescript
-        "html",          -- html
-        "cssls",         -- css
-        "tailwindcss",   -- tailwindcss
-        "lua_ls",        -- lua
-        "pyright",       -- python
-        "ruff",          -- python
+        "ts_ls", -- typescript
+        "html", -- html
+        "cssls", -- css
+        "tailwindcss", -- tailwindcss
+        "lua_ls", -- lua
+        "pyright", -- python
+        "ruff", -- python
         "rust_analyzer", -- rust
-        "jsonls",        -- json
-        "yamlls",        -- yaml
-        "dockerls",      -- docker
-        "marksman",      -- markdown
-        "eslint",        -- js/ts linting
+        "jsonls", -- json
+        "yamlls", -- yaml
+        "dockerls", -- docker
+        "marksman", -- markdown
+        "eslint", -- js/ts linting
       },
       -- Mason-LSPConfig v2 automatically enables servers by default
       automatic_enable = true, -- This is the default, but being explicit
@@ -46,8 +46,9 @@ return {
     mason_tool_installer.setup({
       ensure_installed = {
         "prettier", -- prettier formatter
-        "stylua",   -- lua formatter
-        "ruff",     -- python linter/formatter
+        "stylua", -- lua formatter
+        "ruff", -- python linter/formatter
+        "eslint_d", -- js/ts linter (faster than eslint)
       },
     })
   end,
