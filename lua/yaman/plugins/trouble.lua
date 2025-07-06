bb92d9ffc111132e01a36b8dbdc2472f898914b8
Yaman Ahlawat <yaman@outlook.com>
2025-07-06 13:45:35 +0530

    refactor: icons in trouble.lua

--- lua/yaman/plugins/trouble.lua
+++ lua/yaman/plugins/trouble.lua
@@ -1,6 +1,6 @@
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  dependencies = { "DaikyXendo/nvim-material-icon", "folke/todo-comments.nvim" },
   opts = {
     focus = true,
   },
