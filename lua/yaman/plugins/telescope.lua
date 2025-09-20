60556a7c833354fde41750306d1a2132cac7ba36
Yaman Ahlawat <yaman@outlook.com>
2025-09-20 15:26:03 +0530

    feat: simplified nvim config based on 0.11 version

--- lua/yaman/plugins/telescope.lua
+++ lua/yaman/plugins/telescope.lua
@@ -5,90 +5,30 @@ return {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
   },
   config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-
-    telescope.setup({
+    require("telescope").setup({
       defaults = {
         path_display = { "smart" },
-        layout_config = {
-          prompt_position = "bottom",
-          horizontal = {
-            preview_width = 0.55,
-          },
-          vertical = {
-            mirror = false,
-          },
-          width = 0.87,
-          height = 0.80,
-        },
-        sorting_strategy = "ascending",
-        -- Exclude common directories you don't want to search
         file_ignore_patterns = {
           "%.git/",
           "node_modules/",
           "%.venv/",
-          "venv/",
           "__pycache__/",
-          "%.pytest_cache/",
-          "%.coverage/",
-          "%.ruff_cache/",
-          "build/",
-          "dist/",
-          "target/",
           "%.next/",
-          "%.nuxt/",
           "%.cache/",
           "%.DS_Store",
           "%.lock",
-          "package%-lock%.json",
-          "yarn%.lock",
-          "Cargo%.lock",
-        },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-        -- Let TokyoNight handle the border styling
-        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-      },
-      pickers = {
-        find_files = {
-          -- Show hidden files like .env, .gitignore, etc.
-          hidden = true,
-          -- Don't respect .gitignore to find all hidden files
-          no_ignore = true,
-        },
-        live_grep = {
-          -- Search in hidden files
-          additional_args = function()
-            return { "--hidden" }
-          end,
-        },
-        grep_string = {
-          -- Search in hidden files
-          additional_args = function()
-            return { "--hidden" }
-          end,
         },
       },
     })
 
-    telescope.load_extension("fzf")
+    require("telescope").load_extension("fzf")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>")
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
   end,
 }
--- lua/yaman/plugins/treesitter.lua
+++ lua/yaman/plugins/treesitter.lua
@@ -6,21 +6,10 @@ return {
     "windwp/nvim-ts-autotag",
   },
   config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
+    require("nvim-treesitter.configs").setup({
+      highlight = { enable = true },
       indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
+      autotag = { enable = true },
       ensure_installed = {
         "json",
         "javascript",
@@ -30,26 +19,13 @@ return {
         "html",
         "css",
         "markdown",
-        "markdown_inline",
-        "graphql",
         "bash",
         "lua",
         "vim",
         "dockerfile",
         "gitignore",
-        "query",
-        "vimdoc",
         "python",
-        "rust"
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
+        "rust",
       },
     })
   end,
