66e97fee04e1780cb372e6b3600a091407c6d70a
Yaman Ahlawat <yaman@outlook.com>
2025-09-20 11:54:02 +0530

    refactor: color-scheme and lualine

--- lua/yaman/plugins/lualine.lua
+++ lua/yaman/plugins/lualine.lua
@@ -76,7 +76,6 @@ return {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" }, -- TokyoNight orange color for updates
           },
           { "encoding" },
           { "fileformat" },
