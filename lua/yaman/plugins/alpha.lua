bcf6abc55be3532c7b83a3ca2e4fa1815ef106c2
Yaman Ahlawat <yaman@outlook.com>
2026-06-18 18:28:04 +0530

    feat: updated color scheme, telescope and mason config

--- lua/yaman/plugins/alpha.lua
+++ lua/yaman/plugins/alpha.lua
@@ -21,7 +21,7 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
       dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>lua require('telescope.builtin').find_files({ hidden = true })<CR>"),
       dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
       dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
       dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
